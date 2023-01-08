a93b66eb31f1a1a40951369314aec46dfa8a5e07
40210315 <92998384+40210315@users.noreply.github.com>
2023-01-08 23:19:55 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,174 @@
+# Login
+from tkinter import *
+from tkinter import messagebox
+
+############################ FUNCTIONS ###############################
+def logInPg():
+    login=Toplevel()
+    
+    # Window placement code
+    appWidth = 600
+    appHeight = 250
+
+    x = (screenWidth/2) - (appWidth/2)
+    y = (screenHeight/2) - (appHeight/2)
+
+    login.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
+
+    login.title("Log in")
+    login.resizable(0,0)
+
+    ############################ ELEMENTS ###############################
+
+    # Label Entry Username
+    lbl_username = Label(login, text="Username:", fg="red", font=("TkDefaultFont 11"))
+    lbl_username.place(x=150, y=40)
+
+    # Entry Username
+    txt_username = Entry(login, width=25)
+    txt_username.place(x=250, y=40)
+
+    # Label Entry Password
+    lbl_password = Label(login, text="Password:", fg="red", font=("TkDefaultFont 11"))
+    lbl_password.place(x=150, y=70)
+
+    # Entry Password
+    txt_password = Entry(login, width=25, show="*")
+    txt_password.place(x=250, y=70)
+
+    # Button Log in
+    btn_login = Button(login, text="Log In", fg="blue", relief="raised", bd=3, width=7, height=2)
+    btn_login.place(x=380, y=150)
+
+    # Label Sign up
+    lbl_signUp = Label(login, text="Não tem conta?", fg="blue")
+    lbl_signUp.place(x=245, y=125)
+
+    # Button Sign up
+    btn_signUp = Button(login, text="Criar Conta", fg="blue", relief="raised", bd=3, width=10, height=2)
+    btn_signUp.place(x=250, y=150)
+
+    ############################ MAINLOOP ###############################
+    login.mainloop()
+
+def signUpPg():
+    signup=Toplevel()
+
+    # Window placement code
+    screenWidth = signup.winfo_screenwidth()
+    screenHeight = signup.winfo_screenheight()
+
+    appWidth = 600
+    appHeight = 300
+
+    x = (screenWidth/2) - (appWidth/2)
+    y = (screenHeight/2) - (appHeight/2)
+
+    signup.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
+
+    signup.title("Sign Up")
+    signup.resizable(0,0)
+
+    ############################ ELEMENTS ###############################
+
+    # Label Entry Username
+    lbl_username = Label(signup, text="Username:", fg="red", font=("TkDefaultFont 11"))
+    lbl_username.place(x=170, y=40)
+
+    # Entry Username
+    txt_username = Entry(signup, width=25)
+    txt_username.place(x=270, y=40)
+
+    # Label Entry Password
+    lbl_password = Label(signup, text="Password:", fg="red", font=("TkDefaultFont 11"))
+    lbl_password.place(x=170, y=70)
+
+    # Entry Password
+    txt_password = Entry(signup, width=25, show="*")
+    txt_password.place(x=270, y=70)
+
+    # Label Entry Password
+    lbl_passwordConfirm = Label(signup, text="Password Confirmation:", fg="red", font=("TkDefaultFont 11"))
+    lbl_passwordConfirm.place(x=85, y=100)
+
+    # Entry Password Confirmation
+    txt_passwordConfirm = Entry(signup, width=25, show="*")
+    txt_passwordConfirm.place(x=270, y=100)
+
+    # RadioButton Account Type
+    selected = StringVar()
+    selected.set("user")
+
+    rdbtn_userAcc = Radiobutton(signup, text="Conta User Normal", fg="blue", value="user", variable=selected)
+    rdbtn_userAcc.place(x=170, y=160)
+
+    rdbtn_adminAcc = Radiobutton(signup, text="Conta Admin", fg="blue", value="admin", variable=selected)
+    rdbtn_adminAcc.place(x=170, y=200)
+
+    # Button Sign up
+    btn_signUp = Button(signup, text="Criar Conta", fg="blue", relief="raised", bd=3, width=10, height=5)
+    btn_signUp.place(x=350, y=150)
+
+    ############################ MAINLOOP ###############################
+    signup.mainloop()
+
+############################ WINDOW ###############################
+main=Tk()
+
+# Window placement code
+screenWidth = main.winfo_screenwidth()
+screenHeight = main.winfo_screenheight()
+
+appWidth = 1100
+appHeight = 600
+
+x = (screenWidth/2) - (appWidth/2)
+y = (screenHeight/2) - (appHeight/2)
+
+main.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
+
+main.title("Main Page")
+main.resizable(0,0)
+
+############################ ELEMENTS ###############################
+# Image Canvas
+ctnImage = Canvas(main, width=700, height=800)
+ctnImage.place(x=468, y=152)
+
+img = PhotoImage(file="images\main_img.png")
+ctnImage.create_image(0, 0, anchor="nw", image=img)
+
+# Paned Window Buttons
+panel1 = PanedWindow(main, height=600, width=448, bg="dark grey")
+panel1.place(x=0, y=0)
+
+# Button Log In Page
+btn_Login = Button(main, text="Log In", fg="blue", relief="raised", bd=3, width=15, height=2, font=("TkDefaultFont", "12", "bold"), command=logInPg)
+btn_Login.place(x=720, y=40)
+
+# Button Log In Page
+btn_SignUp = Button(main, text="Sign Up", fg="blue", relief="raised", bd=3, width=15, height=2, font=("TkDefaultFont", "12", "bold"), command=signUpPg)
+btn_SignUp.place(x=890, y=40)
+
+# Button Catalogue
+imageIconCat = PhotoImage(file="images\\catal_img.png")
+btn_cat = Button(panel1, text="Catalogue", fg="blue", relief="raised", bd=3, width=300, height=80, font=("TkDefaultFont", "20", "bold"), image=imageIconCat, compound=RIGHT)
+btn_cat.place(x=80, y=40)
+
+# Button Search
+imageIconSearch = PhotoImage(file="images\\search_img.png")
+btn_ser = Button(panel1, text="Search", fg="blue", relief="raised", bd=3, width=300, height=80, font=("TkDefaultFont", "20", "bold"), image=imageIconSearch, compound=RIGHT)
+btn_ser.place(x=80, y=150)
+
+# Button User Profile
+imageIconUser = PhotoImage(file="images\\user_img.png")
+btn_user = Button(panel1, text="User Profile", fg="blue", relief="raised", bd=3, width=300, height=80, font=("TkDefaultFont", "20", "bold"), image=imageIconUser, compound=RIGHT)
+btn_user.place(x=80, y=260)
+
+# Button Close
+imageIconClose = PhotoImage(file="images\\close_img.png")
+btn_close = Button(panel1, text="Close", fg="blue", relief="raised", bd=3, width=300, height=80, font=("TkDefaultFont", "20", "bold"), image=imageIconClose, compound=RIGHT, command=main.destroy)
+btn_close.place(x=80, y=370)
+
+############################ MAINLOOP ###############################
+main.mainloop()
---
+++ manage_users.py
@@ -0,0 +1,100 @@
+# Manage Users
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+############################ FUNCTIONS ###############################
+
+
+############################ WINDOW ###############################
+manageUsers=Tk()
+
+# Window placement code
+screenWidth = manageUsers.winfo_screenwidth()
+screenHeight = manageUsers.winfo_screenheight()
+
+appWidth = 830
+appHeight = 350
+
+x = (screenWidth/2) - (appWidth/2)
+y = (screenHeight/2) - (appHeight/2)
+
+manageUsers.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
+
+manageUsers.title("Manage Users Page")
+manageUsers.resizable(0,0)
+
+############################ ELEMENTS ###############################
+# Menu Barra
+barra_Menu = Menu(manageUsers)
+
+# Games Menu Cascade
+games_Menu = Menu(barra_Menu)
+games_Menu.add_command(label="Catalogue", command="noaction")
+games_Menu.add_command(label="Search", command="noaction")
+barra_Menu.add_cascade(label="Games", menu=games_Menu)
+
+# User Menu Cascade
+user_Menu = Menu(barra_Menu)
+user_Menu.add_command(label="User Profile", command="noaction")
+user_Menu.add_command(label="Favourites", command="noaction")
+barra_Menu.add_cascade(label="User", menu=user_Menu)
+
+# Admin Menu Cascade
+admin_Menu = Menu(barra_Menu)
+admin_Menu.add_command(label="Manage Users", command="noaction")
+admin_Menu.add_command(label="Manage Games", command="noaction")
+barra_Menu.add_cascade(label="Admin", menu=admin_Menu)
+
+# Close Option
+barra_Menu.add_command(label="Sair", command=manageUsers.quit)
+
+manageUsers.configure(menu=barra_Menu)
+
+# LabelFrame Filter
+frame1 = LabelFrame(manageUsers, text="Filters", fg="red", width=300, height=210, relief="sunken", bd=3)
+frame1.place(x=30, y=10)
+
+# Label Entry Game Name
+lbl_gameName = Label(frame1, text="Username:", fg="blue")
+lbl_gameName.place(x=20, y=30)
+
+# Entry Game Search
+txt_gameName = Entry(frame1, width=25)
+txt_gameName.place(x=110, y=30)
+
+# Radio Button User Type
+selected = StringVar()
+selected.set("user")
+rd1 = Radiobutton(frame1, text="User", value="user", variable=selected)
+rd2 = Radiobutton(frame1, text="Admin", value="admin", variable=selected)
+rd1.place(x=50, y=80)
+rd2.place(x=130, y=80)
+
+# Button Search
+btn_search = Button(frame1, text="Filter", fg="red", width=15, height=2)
+btn_search.place(x=120, y=130)
+
+# Button Delete Game
+btn_del = Button(manageUsers, text="Delete Selected", fg="blue", width=25, height=3)
+btn_del.place(x=80, y= 250)
+
+# Treeview Users
+panel1 = PanedWindow(manageUsers, width=420, height=300, relief="sunken", bd=2)
+panel1.place(x=350, y=20)
+
+global tree
+tree = ttk.Treeview(panel1, selectmode="browse", columns=("User Type", "Username", "Password"), show="headings", height=300)
+
+tree.column("User Type", width=155, anchor="c")
+tree.heading("User Type", text="User Type")
+
+tree.column("Username", width=130, anchor="c")
+tree.heading("Username", text="Username")
+
+tree.column("Password", width=130, anchor="c")
+tree.heading("Password", text="Password")
+
+tree.place(x=0, y=0)
+############################ MAINLOOP ###############################
+manageUsers.mainloop()
